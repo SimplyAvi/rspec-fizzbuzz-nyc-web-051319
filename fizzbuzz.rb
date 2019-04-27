363b1b22b132da19d1494ccc2f5a9a02dd691dc0
Avitosh Totaram <a.totaram@gmail.com>
2019-04-27 05:41:28 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizbuzz(num)
+def fizzbuzz(num)
   if num % 3 == 0 && num % 5 == 0
-     "FizzBuzz"
+    return "FizzBuzz"
   elseif num % 3 == 0 
-     "Fizz"
+    return "Fizz"
   else if num % 5 == 0
-   "Buzz"
+  return "Buzz"
   end
 end
